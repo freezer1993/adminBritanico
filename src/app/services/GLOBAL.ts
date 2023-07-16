e4734cf6b6620774894f2cf9a5176093a3aa8b42
Luis Fleitas <luisfleitas1993@outlook.com>
2023-07-16 19:45:13 -0400

    bk 16/07/2023 16:45

--- src/app/services/GLOBAL.ts
+++ src/app/services/GLOBAL.ts
@@ -1,4 +1,5 @@
 export var GLOBAL  = {
-    url:'http://127.0.0.1:4201/api/',
-    id_sucursal: '5'
+    url:'http://192.168.4.59:4201/api/',
+    id_sucursal: '5',
+    hostApi: '192.168.4.59:4201'
 }
